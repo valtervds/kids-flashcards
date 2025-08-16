162874affbdb98713f6f1418f461d0e3b58f6ef3
Valter Valido <valter.valido@avanade.com>
2025-08-16 20:08:53 -0300

    fix: add base path for github pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,14 @@
 import react from '@vitejs/plugin-react';
 
+const isCI = process.env.GITHUB_ACTIONS === 'true';
+// Em GitHub Pages o app fica em /kids-flashcards/
+const base = isCI ? '/kids-flashcards/' : '/';
+
 export default {
+  base,
   plugins: [react()],
   root: './src',
-  publicDir: '../public', // assets PWA (manifest, sw, icons)
+  publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true
